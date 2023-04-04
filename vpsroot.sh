89f73d72ce57b781c60bebf1b521bd0eebf8f715
Rolka111111 <90156401+Rolka111111@users.noreply.github.com>
2023-04-04 19:14:30 +0300

    Update vpsroot.sh

--- vpsroot.sh
+++ vpsroot.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 # Mod By SL
 #echo "$crot    ALL=(ALL:ALL) ALL" >> /etc/sudoers;
-wget -qO- -O /etc/ssh/sshd_config https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/sshd_config;
+wget -qO- -O /etc/ssh/sshd_config https://raw.githubusercontent.com/Rolka111111/Mantap/main/sshd_config;
 systemctl restart sshd;
 clear;
-echo -e "Masukkan Password:";
+echo -e "Enter Password:";
 read -e pwe;
 usermod -p `perl -e "print crypt("$pwe","Q4")"` root;
 clear;
-printf "Mohon Simpan Informasi Akun VPS Ini
+printf "Please Save This VPS Account Information
 ============================================
 Akun Root (Akun Utama)
 Ip address = $(curl -Ls http://ipinfo.io/ip)
